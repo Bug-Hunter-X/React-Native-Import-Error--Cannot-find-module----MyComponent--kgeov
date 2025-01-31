a310e68ffe5b23f7fa722c8b6c71b6f71ebe2f81
BugHunter-X <askzenai@gmail.com>
2025-01-31 19:16:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This React Native code produces an error because of an incorrect import statement. The component 'MyComponent' is imported from './MyComponent', but the actual file name is 'MyComponent.js'.
