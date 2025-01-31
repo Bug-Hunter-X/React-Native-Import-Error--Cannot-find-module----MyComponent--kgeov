7407c14fa47e1a252d3a9027730c666ae2aae3b9
BugHunter-X <askzenai@gmail.com>
2025-01-31 19:16:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,5 @@
+The corrected code includes the '.js' extension in the import statement.  This explicitly tells the import system to look for a JavaScript file.
+
+```javascript
+import MyComponent from './MyComponent.js';
+```
